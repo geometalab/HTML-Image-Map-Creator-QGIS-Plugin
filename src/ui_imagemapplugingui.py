1d285da837e97b7dfa9df2e2874fe85ab13c2271
Ŝeverin Fritschi <severin.fritschi@hsr.ch>
2017-06-14 16:23:38 +0200

    Finalized resources

--- src/ui_imagemapplugingui.py
+++ src/ui_imagemapplugingui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'html_image_map_creator_dialog_base.ui'
 #
-# Created: Wed Jun 14 15:03:14 2017
+# Created: Wed Jun 14 16:22:14 2017
 #      by: PyQt4 UI code generator 4.10.2
 #
 # WARNING! All changes made in this file will be lost!
